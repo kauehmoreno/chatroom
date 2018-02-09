a4f12365fc9ea446cc7eaa47c7d957c470ff341f
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2018-02-09 16:28:08 -0200

    handle.go: There was a bug with channelMessage subscription which was not calling fallback to client

--- handle.go
+++ handle.go
@@ -1,38 +1,12 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"time"
 
 	r "github.com/dancannon/gorethink"
 	"github.com/mitchellh/mapstructure"
 )
-
-// Channel struct
-type Channel struct {
-	Id   string `json:"id" gorethink:"id,omitempty"`
-	Name string `json:"name" gorethink:"name"`
-}
-
-// User strucut to define user specification
-type User struct {
-	Id   string `gorethink:"id,omitempty"`
-	Name string `gorethink:"name"`
-}
-
-// ChannelMessage struct identify all attr
-type ChannelMessage struct {
-	Id        string    `gorethink:"id,omitempty"`
-	ChannelId string    `gorethink:"channelId`
-	Body      string    `gorethink:"body"`
-	Author    string    `gorethink:"author"`
-	CreatedAt time.Time `gorethink:"createdAt"`
-}
-
-type Message struct {
-	Name string      `json:"name"`
-	Data interface{} `json:"data"`
-}
 
 // iota make it automatically increment value for variables
 // instead of make it channelStop = 0 then userstop = 1 so on...
@@ -42,6 +16,116 @@ const (
 	MessageStop
 )
 
+type Message struct {
+	Name string      `json:"name"`
+	Data interface{} `json:"data"`
+}
+
+type Channel struct {
+	Id   string `json:"id" gorethink:"id,omitempty"`
+	Name string `json:"name" gorethink:"name"`
+}
+
+type User struct {
+	Id   string `gorethink:"id,omitempty"`
+	Name string `gorethink:"name"`
+}
+
+type ChannelMessage struct {
+	Id        string    `gorethink:"id,omitempty"`
+	ChannelId string    `gorethink:"channelId"`
+	Body      string    `gorethink:"body"`
+	Author    string    `gorethink:"author"`
+	CreatedAt time.Time `gorethink:"createdAt"`
+}
+
+func editUser(client *Client, data interface{}) {
+	var user User
+	err := mapstructure.Decode(data, &user)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+		return
+	}
+	client.userName = user.Name
+	go func() {
+		_, err := r.Table("user").
+			Get(client.id).
+			Update(user).
+			RunWrite(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+		}
+	}()
+}
+
+func subscribeUser(client *Client, data interface{}) {
+	go func() {
+		stop := client.NewStopChannel(UserStop)
+		cursor, err := r.Table("user").
+			Changes(r.ChangesOpts{IncludeInitial: true}).
+			Run(client.session)
+
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+			return
+		}
+		changeFeedHelper(cursor, "user", client.send, stop)
+	}()
+}
+
+func unsubscribeUser(client *Client, data interface{}) {
+	client.StopForKey(UserStop)
+}
+
+func addChannelMessage(client *Client, data interface{}) {
+	var channelMessage ChannelMessage
+	err := mapstructure.Decode(data, &channelMessage)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+	}
+	go func() {
+		channelMessage.CreatedAt = time.Now()
+		channelMessage.Author = client.userName
+		err := r.Table("message").
+			Insert(channelMessage).
+			Exec(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+		}
+	}()
+}
+
+func subscribeChannelMessage(client *Client, data interface{}) {
+	go func() {
+		eventData := data.(map[string]interface{})
+		val, ok := eventData["channelId"]
+		if !ok {
+			return
+		}
+		channelId, ok := val.(string)
+		if !ok {
+			return
+		}
+		stop := client.NewStopChannel(MessageStop)
+		// Need a more optimize query..
+		cursor, err := r.Table("message").
+			OrderBy(r.OrderByOpts{Index: r.Desc("createdAt")}).
+			Filter(r.Row.Field("channelId").Eq(channelId)).
+			Changes(r.ChangesOpts{IncludeInitial: true}).
+			Run(client.session)
+
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+			return
+		}
+		changeFeedHelper(cursor, "message", client.send, stop)
+	}()
+}
+
+func unsubscribeChannelMessage(client *Client, data interface{}) {
+	client.StopForKey(MessageStop)
+}
+
 func addChannel(client *Client, data interface{}) {
 	var channel Channel
 	err := mapstructure.Decode(data, &channel)
@@ -49,24 +133,23 @@ func addChannel(client *Client, data interface{}) {
 		client.send <- Message{"error", err.Error()}
 		return
 	}
-	fmt.Println(&channel.Name)
 	// Run it owns goroutine to better performance
 	// goroutine are light weight so use itttttt.
 	go func() {
 		// Exec() doesnt return database result however RunWrite does it
-		erro := r.Table("channel").Insert(channel).Exec(client.session)
-		if erro != nil {
+		err = r.Table("channel").
+			Insert(channel).
+			Exec(client.session)
+		if err != nil {
 			client.send <- Message{"error", err.Error()}
 		}
 	}()
-
 }
 
 func subscribeChannel(client *Client, data interface{}) {
 	// this is a blocking operation, so on run it own goroutine
 	go func() {
-		stop := client.NewStopChannels(ChannelStop)
-		// creating changefeed
+		stop := client.NewStopChannel(ChannelStop)
 		cursor, err := r.Table("channel").
 			Changes(r.ChangesOpts{IncludeInitial: true}).
 			Run(client.session)
@@ -79,43 +162,7 @@ func subscribeChannel(client *Client, data interface{}) {
 }
 
 func unsubscribeChannel(client *Client, data interface{}) {
-	client.StopChannelForKey(ChannelStop)
-}
-
-func editUser(client *Client, data interface{}) {
-	var user User
-
-	err := mapstructure.Decode(data, &user)
-	if err != nil {
-		client.send <- Message{"error", err.Error()}
-		return
-	}
-	client.userName = user.Name
-
-	go func() {
-		_, err := r.Table("user").Get(client.id).Update(user).RunWrite(client.session)
-		if err != nil {
-			client.send <- Message{"error", err.Error()}
-		}
-	}()
-}
-
-func subscribeUser(client *Client, data interface{}) {
-	go func() {
-		stop := client.NewStopChannels(UserStop)
-		cursor, err := r.Table("user").
-			Changes(r.ChangesOpts{IncludeInitial: true}).Run(client.session)
-
-		if err != nil {
-			client.send <- Message{"error", err.Error()}
-			return
-		}
-		changeFeedHelper(cursor, "user", client.send, stop)
-	}()
-}
-
-func unsubscribeUser(client *Client, data interface{}) {
-	client.StopChannelForKey(UserStop)
+	client.StopForKey(ChannelStop)
 }
 
 func changeFeedHelper(cursor *r.Cursor, changeEventName string, send chan<- Message, stop <-chan bool) {
@@ -151,49 +198,4 @@ func changeFeedHelper(cursor *r.Cursor, changeEventName string, send chan<- Mess
 			send <- Message{eventName, data}
 		}
 	}
-}
-
-func addChannelMessage(client *Client, data interface{}) {
-	var channelMessage ChannelMessage
-	err := mapstructure.Decode(data, &channelMessage)
-	if err != nil {
-		client.send <- Message{"error", err.Error()}
-	}
-	go func() {
-		channelMessage.CreatedAt = time.Now()
-		channelMessage.Author = client.userName
-		err := r.Table("message").Insert(channelMessage).Exec(client.session)
-		if err != nil {
-			client.send <- Message{"error", err.Error()}
-		}
-	}()
-}
-
-func subscribeChannelMessage(client *Client, data interface{}) {
-	go func() {
-		eventData := data.(map[string]interface{})
-		val, ok := eventData["channelId"]
-		if !ok {
-			return
-		}
-		channelId, ok := val.(string)
-		if !ok {
-			return
-		}
-		stop := client.NewStopChannels(MessageStop)
-		// Need a more optimize query..
-		cursor, err := r.Table("message").
-			OrderBy(r.OrderByOpts{Index: r.Desc("createdAt")}).
-			Filter(r.Row.Field("channelId").Eq(channelId)).
-			Changes(r.ChangesOpts{IncludeInitial: true}).Run(client.session)
-		if err != nil {
-			client.send <- Message{"error", err.Error()}
-			return
-		}
-		changeFeedHelper(cursor, "message", client.send, stop)
-	}()
-}
-
-func unsubscribeChannelMessage(client *Client, data interface{}) {
-	client.StopChannelForKey(MessageStop)
 }
